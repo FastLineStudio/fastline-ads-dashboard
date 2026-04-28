4083d78432f3750583d1543dbc1899856c809aa6
FastLineStudio <mail@fastline.studio>
2026-04-28 19:50:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ GAQL_DATE_MAP = {
 }
 
 def google_get_access_token():
-        direct_token = st.secrets.get("GOOGLE_ACCESS_TOKEN", "")
+    direct_token = st.secrets.get("GOOGLE_ACCESS_TOKEN", "")
     if direct_token:
         return direct_token
     client_id = st.secrets.get("GOOGLE_CLIENT_ID", "")
